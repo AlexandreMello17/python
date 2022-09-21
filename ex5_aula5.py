2b5e1c5c82f93f836974b73a1ec79787ef27d96a
alx47 <101833866+alx47@users.noreply.github.com>
2022-09-21 00:27:13 -0300

    Create ex5_aula5.py

---
+++ ex5_aula5.py
@@ -0,0 +1,13 @@
+nota1 = float (input("Insira sua primeira nota..: "))
+nota2 = float (input("Insira sua segunda nota..: "))
+
+media = (nota1 + nota2 )/2
+
+if (media > 7 and media < 10 ):
+    print("Aprovado.")
+
+elif (media < 7):
+    print("Reprovado.")
+
+elif (media == 10):
+    print("Aprovado com distinção.")
