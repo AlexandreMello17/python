70339132fc70ebc2d00f4775929985b4eb0bce0c
alx47 <101833866+alx47@users.noreply.github.com>
2022-09-21 00:27:33 -0300

    Create ex7_aula4.py

---
+++ ex7_aula4.py
@@ -0,0 +1,7 @@
+lado1 = int (input ("Valor de um lado do quadrado..: "))
+lado2 = int (input ("Valor do outro lado do quadrado..: "))
+
+aQuadrado = (lado1 * lado2)
+aQuadrado2 = (aQuadrado * 2)
+
+print("O dobro da área do quadrado é igual à..: " , aQuadrado2)
