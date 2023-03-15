68913010621fae59ebd79efdc28d9d9b64b93253
Alexandre Nascimento <101833866+alx47@users.noreply.github.com>
2023-03-15 00:33:05 -0300

    Create ex02_funcao.py

---
+++ ex02_funcao.py
@@ -0,0 +1,9 @@
+def escreva(txt):
+    tamanho = len(txt) + 5
+    print('-' * tamanho)
+    print(f'{txt}')
+    print('-' * tamanho)
+
+escreva('EXERCICIOS PYTHON')
+escreva('EXERCICIOS FUNÇÕES EM PYTHON')
+escreva('ALEXANDRE DEV')
