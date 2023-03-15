9f56aeb27824569f3f8c4241fd881772d62efd46
Alexandre Nascimento <101833866+alx47@users.noreply.github.com>
2023-03-14 23:37:53 -0300

    Create funcao.py

---
+++ funcao.py
@@ -0,0 +1,9 @@
+def titulo(txt):
+    print('-' * 30)
+    print(txt)
+    print('-' * 30)
+
+
+titulo('    SISTEMA DE ALUNOS    ')
+titulo('    CADASTRO DE CLIENTES    ')
+titulo('    ERRO DO SISTEMA    ')
