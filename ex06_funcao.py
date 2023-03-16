67a6a6d28de2617e91b25aa79212ec225d2f9e38
Alexandre Nascimento <101833866+alx47@users.noreply.github.com>
2023-03-16 00:30:30 -0300

    Create ex06_funcao.py

---
+++ ex06_funcao.py
@@ -0,0 +1,22 @@
+def voto(ano_nascimento):
+    from datetime import date
+    ano_atual = date.today().year
+    idade = ano_atual - ano_nascimento
+
+
+    if idade >= 18:
+        print(f'Você possui {idade} anos, logo seu voto é OBRIGATÓRIO.')
+
+
+    if idade >= 65:
+        print(f'Você possui {idade} anos, logo seu voto é OPCIONAL.')   
+
+
+    if idade < 16:
+        print(f'Você possui {idade} anos, logo seu voto é NEGADO.')
+
+    if 16 <= idade < 18:
+        print(f'Você possui {idade} anos, logo seu voto é OPCIONAL.')  
+
+ano_nascimento = int(input('Digite o seu ano de nascimento:  '))
+voto(ano_nascimento)
